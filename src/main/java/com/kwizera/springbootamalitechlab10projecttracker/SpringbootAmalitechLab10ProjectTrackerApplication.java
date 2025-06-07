9c8702788e01655aff126eac6e997767b50f67be
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2025-06-07 21:31:34 +0200

    init commit

---
+++ src/main/java/com/kwizera/springbootamalitechlab10projecttracker/SpringbootAmalitechLab10ProjectTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.kwizera.springbootamalitechlab10projecttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAmalitechLab10ProjectTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAmalitechLab10ProjectTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kwizera/springbootamalitechlab10projecttracker/SpringbootAmalitechLab10ProjectTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwizera.springbootamalitechlab10projecttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAmalitechLab10ProjectTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
