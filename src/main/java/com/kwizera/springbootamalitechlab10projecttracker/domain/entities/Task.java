fb5fa5b95a56490d17338980ca44cf38884ee52b
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2025-06-08 11:42:28 +0200

    defines all entities needed by the persistence layer

---
+++ src/main/java/com/kwizera/springbootamalitechlab10projecttracker/domain/entities/Task.java
@@ -0,0 +1,39 @@
+package com.kwizera.springbootamalitechlab10projecttracker.domain.entities;
+
+import com.kwizera.springbootamalitechlab10projecttracker.domain.enums.StatusEnum;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusEnum status;
+
+    @Column(nullable = false)
+    private LocalDate dueDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id",nullable = false)
+    private Project project;
+
+}
---
+++ src/main/java/com/kwizera/springbootamalitechlab10projecttracker/domain/enums/StatusEnum.java
@@ -0,0 +1,5 @@
+package com.kwizera.springbootamalitechlab10projecttracker.domain.enums;
+
+public enum StatusEnum {
+    COMPLETED, PENDING, CANCELLED
+}
